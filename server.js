51e2b8e7b3f5dbc72929b8ed7db28fe31ab4c44a
Persevere <Persevere@perseverenow.org>
2020-09-21 08:50:57 -0700

    database added

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
     res.send("hello");
 });
+
+mongoose.connect(process.env.DATABASE_URI || process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, () => {
+    console.log('The goose is loose baby');
+})
 
 app.listen(PORT, () => {
     console.log(`app listening on ${PORT}`);
