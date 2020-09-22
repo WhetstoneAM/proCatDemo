8ed8f10a42aca8da6ee64a637fbb199bb53f557a
Persevere <Persevere@perseverenow.org>
2020-09-22 06:27:19 -0700

    ready for day two

--- server.js
+++ server.js
@@ -6,8 +6,26 @@ const app = express();
 const PORT = process.env.PORT;
 const mongoose = require('mongoose');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/waterbearimage', (req, res) => {
+    res.sendFile(__dirname + '/public/other.html')
+});
+
+app.post('/waterbearimage', (req, res) => {
+    let name = req.body.name;
+    let age = req.body.age;
+    let data = [];
+    data.push(name, age);
+    console.log(data);
+    res.sendFile(__dirname + '/public/other.html')
 });
 
 mongoose.connect(process.env.DATABASE_URI || process.env.MONGODB_URI, {
