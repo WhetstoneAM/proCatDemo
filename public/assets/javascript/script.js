94fd37b1a7840aade127b33c76b0a4368b37bbc0
Persevere <Persevere@perseverenow.org>
2020-09-22 08:22:19 -0700

    end of section two

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,1 +1,7 @@
-console.log("I'm here");
+document.addEventListener("DOMContentLoaded", function(){
+    const waterbearName = document.getElementById('waterbearName');
+    const waterbearAge = document.getElementById('waterbearAge');
+    let name = waterbearName.getAttribute('data-name');
+    let age = waterbearAge.getAttribute('data-age');
+    console.log(name, age);
+});
--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const mongoose = require('mongoose');
+const layouts = require('express-ejs-layouts');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static('public'));
+app.use(layouts);
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/page');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
-app.get('/waterbearimage', (req, res) => {
-    res.sendFile(__dirname + '/public/other.html')
+    res.render('layouts/form')
 });
 
 app.post('/waterbearimage', (req, res) => {
@@ -25,7 +26,7 @@ app.post('/waterbearimage', (req, res) => {
     let data = [];
     data.push(name, age);
     console.log(data);
-    res.sendFile(__dirname + '/public/other.html')
+    res.render('layouts/other', { name, age })
 });
 
 mongoose.connect(process.env.DATABASE_URI || process.env.MONGODB_URI, {
